b01b2ba80f60479dcf1415c50ba300ba2fdfb8c5
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2025-09-21 23:43:47 -0300

    Adding SearchParams class

--- src/shared/domain/repositories/serchable-repository-contracts.ts
+++ src/shared/domain/repositories/serchable-repository-contracts.ts
@@ -19,6 +19,94 @@
 import { Entity } from '../entities/entity'
 import { RepositoryInterface } from './repository-contracts'
 
+export type SortDirection = 'asc' | 'desc'
+
+export type SearchProps<Filter = string> = {
+  page?: number
+  perPage?: number
+  sort?: string | null
+  sortDir?: SortDirection | null
+  filter?: Filter | null
+}
+
+export class SearchParams {
+  protected _page: number
+  protected _perPage: number
+  protected _sort: string | null
+  protected _sortDir: SortDirection | null
+  protected _filter: string | null
+
+  constructor(props: SearchProps) {
+    this._page = props.page ?? 1
+    this._perPage = props.perPage ?? 15
+    this._sort = props.sort ?? null
+    this._sortDir = props.sortDir ?? null
+    this._filter = props.filter ?? null
+  }
+
+  private set page(value: number) {
+    let _page = +value
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) != _page) {
+      _page = 1
+    }
+    this._page = _page
+  }
+
+  get page(): number {
+    return this._page
+  }
+
+  private set perPage(value: number) {
+    let _perPage = +value
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      parseInt(_perPage as any) != _perPage
+    ) {
+      _perPage = this._perPage
+    }
+    this._perPage = _perPage
+  }
+
+  get perPage(): number {
+    return this._perPage
+  }
+
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`
+  }
+
+  get sort(): string | null {
+    return this._sort
+  }
+
+  private set sortDir(value: string | null) {
+    if (!this.sort) {
+      this._sortDir = null
+      return
+    }
+    const dir = `${value}`.toLowerCase()
+    this._sortDir =
+      dir !== 'asc' && dir !== 'desc' ? 'asc' : (dir as SortDirection)
+  }
+
+  get sortDir(): SortDirection | null {
+    return this._sortDir
+  }
+
+  private set filter(value: string | null) {
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`
+  }
+
+  get filter(): string | null {
+    return this._filter
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,
