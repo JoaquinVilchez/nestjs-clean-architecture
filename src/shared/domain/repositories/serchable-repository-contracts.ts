26ebbc82652d9242ed33791d1c45e4364b125562
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2025-09-25 15:26:20 -0300

    Adding SearchResults class

--- src/shared/domain/repositories/serchable-repository-contracts.ts
+++ src/shared/domain/repositories/serchable-repository-contracts.ts
@@ -29,6 +29,17 @@ export type SearchProps<Filter = string> = {
   filter?: Filter | null
 }
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[]
+  total: number
+  currentPage: number
+  perPage: number
+  lastPage: number
+  sort: string | null
+  sortDir: SortDirection | null
+  filter: Filter | null
+}
+
 export class SearchParams {
   protected _page: number
   protected _perPage: number
@@ -121,6 +132,41 @@ export class SearchParams {
 
   get filter(): string | null {
     return this._filter
+  }
+}
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[]
+  readonly total: number
+  readonly currentPage: number
+  readonly perPage: number
+  readonly lastPage: number
+  readonly sort: string | null
+  readonly sortDir: SortDirection | null
+  readonly filter: Filter | null
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items
+    this.total = props.total
+    this.currentPage = props.currentPage
+    this.perPage = props.perPage
+    this.lastPage = Math.ceil(this.total / this.perPage)
+    this.sort = props.sort ?? null
+    this.sortDir = props.sortDir ?? null
+    this.filter = props.filter ?? null
+  }
+
+  toJSON(forceEntity = false) {
+    return {
+      items: forceEntity ? this.items.map(item => item.toJSON()) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    }
   }
 }
 
